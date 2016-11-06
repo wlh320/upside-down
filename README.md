d7555a385b58d868a6164e6f0a80445f7211e713
0h Wang <0h Wang>
2016-11-06 15:38:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+## uʍop-ǝpısdn
+
+- A simple python script to make a sentence upside-down.
+
+- Already supported:zyxwvutsrqponmlkjihgfedcbaVRFBCDEFJMW.?!,\'/><(){}^;_
+
+- Some capital letter is not supported.
+
+- I suggest you to use [Noto fonts](https://www.google.com/get/noto/)
+
+- 。文中持支不还前目
+
+- p.s. 看着好玩瞎搞的，写完才发现github上已经有大把这样的项目了
+
+  ​
---
+++ upsidedown.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# author: W0h
+# just for fun!
+ori = 'zyxwvutsrqponmlkjihgfedcbaVRFBCDEFJMW.?!,\'/\\><(){}^;_'
+ud = 'zʎxʍʌnʇsɹbdouɯןʞɾıɥbɟǝpɔqɐɅᴚℲᗺƆᗡƎᖵſWM˙¿¡\',\\/<>)(}{ᵥ؛‾'
+same = ' HIOXS(|:#=+-*~'
+s = input('input a string:')
+s = s[-1::-1]
+res = ''
+for i in s:
+    if i in same:
+        res += i
+    elif i in ori:
+        res += ud[ori.index(i)]
+    else:  # cannot transform
+        res += '#'
+print(res)
